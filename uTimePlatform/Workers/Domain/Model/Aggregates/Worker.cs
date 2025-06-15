f216f8d7bd174525ed5f22847cdc702aa1dd9e9b
Marcelo <166082569+VarBus@users.noreply.github.com>
2025-06-15 12:09:10 -0500

    feat: add worker structure

--- uTimePlatform/Workers/Domain/Model/Aggregates/Worker.cs
+++ uTimePlatform/Workers/Domain/Model/Aggregates/Worker.cs
@@ -4,7 +4,25 @@ using uTimePlatform.Workers.Domain.Model.ValueObjects;
 
 namespace uTimePlatform.Workers.Domain.Model.Aggregates;
 
-public class Worker
-{
-    
+public class Workers {
+    private long id;
+    private String name;
+    private String specialization;
+    private String photoUrl;
+
+    public Workers(long id, String name, String specialization, String photoUrl) {
+        this.id = id;
+        this.name = name;
+        this.specialization = specialization;
+        this.photoUrl = photoUrl;
+    }
+
+    public long getId() { return id; }
+    public String getName() { return name; }
+    public String getSpecialization() { return specialization; }
+    public String getPhotoUrl() { return photoUrl; }
+
+    public void setName(String name) { this.name = name; }
+    public void setSpecialization(String specialization) { this.specialization = specialization; }
+    public void setPhotoUrl(String photoUrl) { this.photoUrl = photoUrl; }
 }
